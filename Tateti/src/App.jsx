8e9f8d1255c31f0443b8f3b03e6e8c12ac238110
Negro_13 <141974651+Negro-13@users.noreply.github.com>
2024-12-11 17:21:23 -0300

    hola

--- Tateti/src/App.jsx
+++ Tateti/src/App.jsx
@@ -1,26 +1,72 @@
-import { useState, useRef } from 'react'
+import { useState, useRef } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  let player = useRef(true)
-  const [tablero, setTablero] = useState([null, null, null, null, null, null, null, null, null])
+  let player = useRef(true);
+  const [tablero, setTablero] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  // Se llena en tablero de juego
   const prueba = (i) => {
     if (tablero[i] == null) {
-      if (player.current) {
-        tablero[i] = setTablero('X');
-        player.current = false;
-      } else {
-        tablero[i] = setTablero('O')
-        player.current = true;
+      const nuevoTablero = [...tablero];
+      if (winX() != true && winO() != true) {
+        if (player.current) {
+          nuevoTablero[i] = "X";
+          player.current = false;
+        } else {
+          nuevoTablero[i] = "O";
+          player.current = true;
+        }
+        setTablero(nuevoTablero);
       }
     } else {
-      console.log('Ya hay valor');
+      window.alert("Ya se a jugado en esta casilla elija otra");
     }
-  }
+  };
+
+  const winX = () => {
+    if ((tablero[0] == "X" && tablero[1] == "X" && tablero[2] == "X") ||
+      (tablero[3] == "X" && tablero[4] == "X" && tablero[5] == "X") ||
+      (tablero[6] == "X" && tablero[7] == "X" && tablero[8] == "X") ||
+      (tablero[0] == "X" && tablero[3] == "X" && tablero[6] == "X") ||
+      (tablero[1] == "X" && tablero[4] == "X" && tablero[7] == "X") ||
+      (tablero[2] == "X" && tablero[5] == "X" && tablero[8] == "X") ||
+      (tablero[0] == "X" && tablero[4] == "X" && tablero[8] == "X") ||
+      (tablero[2] == "X" && tablero[4] == "X" && tablero[6] == "X")) {
+      window.alert("El jugador X a ganado");
+      return true;
+    }
+  };
+  const winO = () => {
+    if ((tablero[0] == "O" && tablero[1] == "O" && tablero[2] == "O") ||
+      (tablero[3] == "O" && tablero[4] == "O" && tablero[5] == "O") ||
+      (tablero[6] == "O" && tablero[7] == "O" && tablero[8] == "O") ||
+      (tablero[0] == "O" && tablero[3] == "O" && tablero[6] == "O") ||
+      (tablero[1] == "O" && tablero[4] == "O" && tablero[7] == "O") ||
+      (tablero[2] == "O" && tablero[5] == "O" && tablero[8] == "O") ||
+      (tablero[0] == "O" && tablero[4] == "O" && tablero[8] == "O") ||
+      (tablero[2] == "O" && tablero[4] == "O" && tablero[6] == "O")) {
+      window.alert("El jugador O ha ganado");
+      return true;
+    }
+  };
+  const reStart = () => {
+    window.location.reload();
+  };
   return (
     <>
-      <h1>Tateti del Negro</h1>
+      <h1 className="title">Tateti del Negro</h1>
       <div className="tablero">
         <div className="celda" onClick={()=>prueba(0)}>
           <h1>{tablero[0]}</h1>
@@ -28,30 +74,31 @@ function App() {
         <div className="celda" onClick={()=>prueba(1)}>
           <h1>{tablero[1] }</h1>
         </div>
-        <div className="celda">
+        <div className="celda" onClick={()=>prueba(2)}>
           <h1>{tablero[2]}</h1>
         </div>
-        <div className="celda">
+        <div className="celda" onClick={()=>prueba(3)}>
           <h1>{tablero[3]}</h1>
         </div>
-        <div className="celda">
+        <div className="celda" onClick={()=>prueba(4)}>
           <h1>{tablero[4]}</h1>
         </div>
-        <div className="celda">
+        <div className="celda" onClick={()=>prueba(5)}>
           <h1>{tablero[5]}</h1>
         </div>
-        <div className="celda">
+        <div className="celda" onClick={()=>prueba(6)}>
           <h1>{tablero[6]}</h1>
         </div>
-        <div className="celda">
-          <h1>{tablero[6]}</h1>
+        <div className="celda" onClick={()=>prueba(7)}>
+          <h1>{tablero[7]}</h1>
         </div>
-        <div className="celda">
-          <h1>{tablero[6]}</h1>
+        <div className="celda" onClick={()=>prueba(8)}>
+          <h1>{tablero[8]}</h1>
         </div>
       </div>
+      <button onClick={reStart} className="boton">Reiniciar</button>
     </>
   )
 }
 
-export default App
+export default App;
